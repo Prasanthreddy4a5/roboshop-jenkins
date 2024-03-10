874b4ead2e58800715a834e3339ccb51ed8a3db8
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-03-10 13:14:44 +0530

    Jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,25 +1,72 @@
+//def call() {
+//
+//    node('Workstation') {
+//        stage('Compile Code') {
+//            common.compile()
+//        }
+//
+//        stage('Test') {
+//            print 'Hello'
+//        }
+//
+//        stage('Code Quality') {
+//            print 'Hello'
+//        }
+//
+//        stage('Code Security') {
+//            print 'Hello'
+//        }
+//
+//        stage('Release') {
+//            print 'Hello'
+//        }
+//    }
+//
+//}
 def call() {
+    pipeline {
+        agent any
 
-    node('Workstation') {
-        stage('Compile Code') {
-            common.compile()
-        }
+        stages {
+            stage('Compile Code') {
+                steps {
+                    sh 'env'
+                }
+            }
 
-        stage('Test') {
-            print 'Hello'
-        }
+            stage('Test Code') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
 
-        stage('Code Quality') {
-            print 'Hello'
-        }
+            stage('Code Quality') {
+                when {
+                    expression { BRANCH_NAME == "main" }
 
-        stage('Code Security') {
-            print 'Hello'
-        }
+                }
+                steps {
+                    echo 'Hello World'
+                }
+            }
 
-        stage('Release') {
-            print 'Hello'
+            stage('Code Security') {
+                when {
+                    expression { BRANCH_NAME == "main" }
+                }
+                steps {
+                    sh 'env'
+                    echo 'Hello World'
+                }
+            }
+
+            stage('Release') {
+                steps {
+                    sh 'env'
+                    echo 'Hello World'
+                }
+            }
+
         }
     }
-
 }
